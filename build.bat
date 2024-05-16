45e37a635adc359806978b5b33ecd3f7ea942906
Rim <anonymous@mail.com>
2024-05-16 17:18:27 -0400

    maint: fix release

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" build\shield-development.sln /p:Configuration=Debug /p:Platform=x64
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" build\shield-development.sln /p:Configuration=Release /p:Platform=x64
 
 pause
