eb753fb507d018223cffd1b6446f764232c345f3
Rim <info@rimmyscorner.com>
2025-05-21 17:13:02 -0400

    format(premake5.lua): format premake

--- premake5.lua
+++ premake5.lua
@@ -37,7 +37,7 @@ function vertonumarr(value, vernumber, partscount)
 end
 
 dependencies = {
-	basePath = "./deps"
+	basePath = "./deps",
 }
 
 function dependencies.load()
@@ -52,7 +52,7 @@ end
 
 function dependencies.imports()
 	for i, proj in pairs(dependencies) do
-		if type(i) == 'number' then
+		if type(i) == "number" then
 			proj.import()
 		end
 	end
@@ -60,40 +60,40 @@ end
 
 function dependencies.projects()
 	for i, proj in pairs(dependencies) do
-		if type(i) == 'number' then
+		if type(i) == "number" then
 			proj.project()
 		end
 	end
 end
 
-newoption {
+newoption({
 	trigger = "copy-to",
 	description = "Optional, copy the EXE to a custom folder after build, define the path here if wanted.",
-	value = "PATH"
-}
+	value = "PATH",
+})
 
-newoption {
+newoption({
 	trigger = "dev-build",
-	description = "Enable development builds of the client."
-}
+	description = "Enable development builds of the client.",
+})
 
-newoption {
+newoption({
 	trigger = "ci-build",
-	description = "Enable CI builds of the client."
-}
+	description = "Enable CI builds of the client.",
+})
 
-newaction {
+newaction({
 	trigger = "version",
 	description = "Returns the version string for the current commit of the source code.",
 	onWorkspace = function(wks)
 		-- get current version via git
 		local proc = assert(io.popen(gitVersioningCommand, "r"))
-		local gitDescribeOutput = assert(proc:read('*a')):gsub("%s+", "")
+		local gitDescribeOutput = assert(proc:read("*a")):gsub("%s+", "")
 		proc:close()
 		local version = gitDescribeOutput
 
 		proc = assert(io.popen(gitCurrentBranchCommand, "r"))
-		local gitCurrentBranchOutput = assert(proc:read('*a')):gsub("%s+", "")
+		local gitCurrentBranchOutput = assert(proc:read("*a")):gsub("%s+", "")
 		local gitCurrentBranchSuccess = proc:close()
 		if gitCurrentBranchSuccess then
 			-- We got a branch name, check if it is a feature branch
@@ -104,10 +104,10 @@ newaction {
 
 		print(version)
 		os.exit(0)
-	end
-}
+	end,
+})
 
-newaction {
+newaction({
 	trigger = "generate-buildinfo",
 	description = "Sets up build information file like version.h.",
 	onWorkspace = function(wks)
@@ -115,20 +115,20 @@ newaction {
 		local oldVersion = "(none)"
 		local oldVersionHeader = io.open(wks.location .. "/s/version.h", "r")
 		if oldVersionHeader ~= nil then
-			local oldVersionHeaderContent = assert(oldVersionHeader:read('*l'))
+			local oldVersionHeaderContent = assert(oldVersionHeader:read("*l"))
 			while oldVersionHeaderContent do
 				m = string.match(oldVersionHeaderContent, "#define GIT_DESCRIBE (.+)%s*$")
 				if m ~= nil then
-						oldVersion = m
+					oldVersion = m
 				end
 
-				oldVersionHeaderContent = oldVersionHeader:read('*l')
+				oldVersionHeaderContent = oldVersionHeader:read("*l")
 			end
 		end
 
 		-- get current version via git
 		local proc = assert(io.popen(gitVersioningCommand, "r"))
-		local gitDescribeOutput = assert(proc:read('*a')):gsub("%s+", "")
+		local gitDescribeOutput = assert(proc:read("*a")):gsub("%s+", "")
 		proc:close()
 
 		-- generate version.hpp with a revision number if not equal
@@ -137,31 +137,35 @@ newaction {
 			-- get current git hash and write to version.txt (used by the preliminary updater)
 			-- TODO - remove once proper updater and release versioning exists
 			local proc = assert(io.popen("git rev-parse HEAD", "r"))
-			local gitCommitHash = assert(proc:read('*a')):gsub("%s+", "")
+			local gitCommitHash = assert(proc:read("*a")):gsub("%s+", "")
 			proc:close()
 
 			-- get whether this is a clean revision (no uncommitted changes)
 			proc = assert(io.popen("git status --porcelain", "r"))
-			local revDirty = (assert(proc:read('*a')) ~= "")
-			if revDirty then revDirty = 1 else revDirty = 0 end
+			local revDirty = (assert(proc:read("*a")) ~= "")
+			if revDirty then
+				revDirty = 1
+			else
+				revDirty = 0
+			end
 			proc:close()
 
 			-- get current tag name
 			proc = assert(io.popen("git describe --tags --abbrev=0"))
-			local tagName = proc:read('*l')
+			local tagName = proc:read("*l")
 
 			-- get current branch name
 			proc = assert(io.popen("git branch --show-current"))
-			local branchName = proc:read('*l')
+			local branchName = proc:read("*l")
 
 			-- get current build date/time
 			local buildDate = os.date("%Y-%m-%d")
 			local buildTime = os.date("%H:%M:%S")
 
 			-- branch for ci
-			if branchName == nil or branchName == '' then
+			if branchName == nil or branchName == "" then
 				proc = assert(io.popen("git show -s --pretty=%d HEAD"))
-				local branchInfo = proc:read('*l')
+				local branchInfo = proc:read("*l")
 				m = string.match(branchInfo, ".+,.+, ([^)]+)")
 				if m ~= nil then
 					branchName = m
@@ -176,9 +180,9 @@ newaction {
 
 			-- get revision number via git
 			local proc = assert(io.popen("git rev-list --count HEAD", "r"))
-			local revNumber = assert(proc:read('*a')):gsub("%s+", "")
+			local revNumber = assert(proc:read("*a")):gsub("%s+", "")
 
-			print ("Update " .. oldVersion .. " -> " .. gitDescribeOutputQuoted)
+			print("Update " .. oldVersion .. " -> " .. gitDescribeOutputQuoted)
 
 			-- write to version.txt for preliminary updater
 			-- NOTE - remove this once we have a proper updater and proper release versioning
@@ -203,10 +207,18 @@ newaction {
 			versionHeader:write("#define BUILD_TIME " .. cstrquote(buildTime) .. "\n")
 			versionHeader:write("\n")
 			versionHeader:write("// Version transformed for RC files\n")
-			versionHeader:write("#define VERSION_PRODUCT_RC " .. table.concat(vertonumarr(tagName, revNumber, 3), ",") .. "\n")
-			versionHeader:write("#define VERSION_PRODUCT " .. cstrquote(table.concat(vertonumarr(tagName, revNumber, 3), ".")) .. "\n")
-			versionHeader:write("#define VERSION_FILE_RC " .. table.concat(vertonumarr(tagName, revNumber, 4), ",") .. "\n")
-			versionHeader:write("#define VERSION_FILE " .. cstrquote(table.concat(vertonumarr(tagName, revNumber, 4), ".")) .. "\n")
+			versionHeader:write(
+				"#define VERSION_PRODUCT_RC " .. table.concat(vertonumarr(tagName, revNumber, 3), ",") .. "\n"
+			)
+			versionHeader:write(
+				"#define VERSION_PRODUCT " .. cstrquote(table.concat(vertonumarr(tagName, revNumber, 3), ".")) .. "\n"
+			)
+			versionHeader:write(
+				"#define VERSION_FILE_RC " .. table.concat(vertonumarr(tagName, revNumber, 4), ",") .. "\n"
+			)
+			versionHeader:write(
+				"#define VERSION_FILE " .. cstrquote(table.concat(vertonumarr(tagName, revNumber, 4), ".")) .. "\n"
+			)
 			versionHeader:write("\n")
 			versionHeader:write("// Alias definitions\n")
 			versionHeader:write("#define VERSION GIT_DESCRIBE\n")
@@ -219,140 +231,141 @@ newaction {
 			versionHeader:write(" *\n")
 			versionHeader:write(" * This file exists for reasons of complying with our coding standards.\n")
 			versionHeader:write(" *\n")
-			versionHeader:write(" * The Resource Compiler will ignore any content from C++ header files if they're not from STDInclude.hpp.\n")
+			versionHeader:write(
+				" * The Resource Compiler will ignore any content from C++ header files if they're not from STDInclude.hpp.\n"
+			)
 			versionHeader:write(" * That's the reason why we now place all version info in version.h instead.\n")
 			versionHeader:write(" */\n")
 			versionHeader:write("\n")
 			versionHeader:write("#include \".\\version.h\"\n")
 			versionHeader:close()
 		end
-	end
-}
+	end,
+})
 
 --DEFUALT
 
 dependencies = {
-    basePath = "./deps"
+	basePath = "./deps",
 }
 
 function dependencies.load()
-    dir = path.join(dependencies.basePath, "premake/*.lua")
-    deps = os.matchfiles(dir)
+	dir = path.join(dependencies.basePath, "premake/*.lua")
+	deps = os.matchfiles(dir)
 
-    for i, dep in pairs(deps) do
-        dep = dep:gsub(".lua", "")
-        require(dep)
-    end
+	for i, dep in pairs(deps) do
+		dep = dep:gsub(".lua", "")
+		require(dep)
+	end
 end
 
 function dependencies.imports()
-    for i, proj in pairs(dependencies) do
-        if type(i) == 'number' then
-            proj.import()
-        end
-    end
+	for i, proj in pairs(dependencies) do
+		if type(i) == "number" then
+			proj.import()
+		end
+	end
 end
 
 function dependencies.projects()
-    for i, proj in pairs(dependencies) do
-        if type(i) == 'number' then
-            proj.project()
-        end
-    end
+	for i, proj in pairs(dependencies) do
+		if type(i) == "number" then
+			proj.project()
+		end
+	end
 end
 
 dependencies.load()
 
+workspace("t8-mod")
+startproject("client")
+location("./build")
+objdir("%{wks.location}/obj")
+targetdir("%{wks.location}/bin/%{cfg.platform}/%{cfg.buildcfg}")
 
-workspace "t8-mod"
-    startproject "client"
-    location "./build"
-    objdir "%{wks.location}/obj"
-    targetdir "%{wks.location}/bin/%{cfg.platform}/%{cfg.buildcfg}"
+configurations({ "Debug", "Release" })
 
-    configurations {"Debug", "Release"}
+language("C++")
+cppdialect("C++20")
 
-    language "C++"
-    cppdialect "C++20"
+architecture("x86_64")
+platforms("x64")
 
-    architecture "x86_64"
-    platforms "x64"
+systemversion("latest")
+symbols("On")
+staticruntime("On")
+editandcontinue("Off")
+warnings("Extra")
+characterset("ASCII")
 
-    systemversion "latest"
-    symbols "On"
-    staticruntime "On"
-    editandcontinue "Off"
-    warnings "Extra"
-    characterset "ASCII"
+if _OPTIONS["dev-build"] then
+	defines({ "DEV_BUILD" })
+end
 
-    if _OPTIONS["dev-build"] then
-        defines {"DEV_BUILD"}
-    end
+if _OPTIONS["ci-build"] then
+	defines({ "CI" })
+end
 
-    if _OPTIONS["ci-build"] then
-        defines {"CI"}
-    end
+flags({ "NoIncrementalLink", "NoMinimalRebuild", "MultiProcessorCompile", "No64BitChecks" })
 
-    flags {"NoIncrementalLink", "NoMinimalRebuild", "MultiProcessorCompile", "No64BitChecks"}
+filter("platforms:x64")
+defines({ "_WINDOWS", "WIN32" })
+filter({})
 
-    filter "platforms:x64"
-        defines {"_WINDOWS", "WIN32"}
-    filter {}
+filter("configurations:Release")
+optimize("Size")
+buildoptions({ "/GL" })
+linkoptions({ "/IGNORE:4702", "/LTCG" })
+defines({ "NDEBUG" })
+flags({ "FatalCompileWarnings" })
+warnings("off")
+filter({})
 
-    filter "configurations:Release"
-        optimize "Size"
-        buildoptions {"/GL"}
-        linkoptions { "/IGNORE:4702", "/LTCG" }
-        defines {"NDEBUG"}
-        flags {"FatalCompileWarnings"}
-        warnings "off"
-    filter {}
+filter("configurations:Debug")
+optimize("Debug")
+defines({ "DEBUG", "_DEBUG" })
+filter({})
 
-    filter "configurations:Debug"
-        optimize "Debug"
-        defines {"DEBUG", "_DEBUG"}
-    filter {}
+project("shared")
+kind("StaticLib")
+language("C++")
 
-project "shared"
-    kind "StaticLib"
-    language "C++"
+files({ "./src/shared/**.hpp", "./src/shared/**.cpp" })
 
-    files {"./src/shared/**.hpp", "./src/shared/**.cpp"}
+includedirs({ "./src/shared", "%{prj.location}/src" })
 
-    includedirs {"./src/shared", "%{prj.location}/src"}
+resincludedirs({ "$(ProjectDir)src" })
 
-    resincludedirs {"$(ProjectDir)src"}
+dependencies.imports()
 
-    dependencies.imports()
+disablewarnings({ "4244" })
 
-	disablewarnings { "4244" }
+project("client")
+kind("SharedLib")
+language("C++")
 
-project "client"
-    kind "SharedLib"
-    language "C++"
+targetname("XInput9_1_0")
 
-    targetname "XInput9_1_0"
+pchheader("std_include.hpp")
+pchsource("./src/client/std_include.cpp")
 
-    pchheader "std_include.hpp"
-    pchsource "./src/client/std_include.cpp"
+files({ "./src/client/**.rc", "./src/client/**.hpp", "./src/client/**.cpp", "./src/client/resources/**.*" })
 
-    files {"./src/client/**.rc", "./src/client/**.hpp", "./src/client/**.cpp", "./src/client/resources/**.*"}
+includedirs({ "./src/client", "./src/shared", "%{prj.location}/src" })
 
-    includedirs {"./src/client", "./src/shared", "%{prj.location}/src"}
+resincludedirs({ "$(ProjectDir)src" })
 
-    resincludedirs {"$(ProjectDir)src"}
+links({ "shared" })
 
-    links {"shared"}
+if _OPTIONS["copy-to"] then
+	postbuildcommands({ "copy /y \"$(TargetPath)\" \"" .. _OPTIONS["copy-to"] .. "\"" })
+end
 
-    if _OPTIONS["copy-to"] then
-        postbuildcommands {"copy /y \"$(TargetPath)\" \"" .. _OPTIONS["copy-to"] .. "\""}
-    end
+dependencies.imports()
 
-    dependencies.imports()
+disablewarnings({ "4244" })
 
-	disablewarnings { "4244" }
+group("Dependencies")
+dependencies.projects()
 
-group "Dependencies"
-    dependencies.projects()
-
-prebuildcommands {"pushd %{_MAIN_SCRIPT_DIR}", "tools\\premake5 generate-buildinfo", "popd"}
+prebuildcommands({ "pushd %{_MAIN_SCRIPT_DIR}", "tools\\premake5 generate-buildinfo", "popd" })
